f152016e8f7baa4edf49979a46b625ccba935584
Davide Luppoli <davide@davideluppoli.it>
2024-10-17 11:31:49 +0200

    Controller getAll

---
+++ Controllers/StudentiController.cs
@@ -0,0 +1,25 @@
+﻿using Studenti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studenti.Controllers
+{
+    internal class StudentiController
+    {
+        private List<Studente> archivio;
+        public StudentiController()
+        {
+            archivio.Add(new Studente("M001", "Rossi", "Mario"));
+            archivio.Add(new Studente("M002", "Verdi", "Luigi"));
+        }
+
+        public List<Studente> getAll() { 
+        
+            //SELECT * FROM Studenti
+            return archivio;
+        }
+    }
+}
