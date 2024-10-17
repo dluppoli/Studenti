4e0c54ee615173253daa2a46d2a37b1ae0f37990
Davide Luppoli <davide@davideluppoli.it>
2024-10-17 11:29:43 +0200

    Model

---
+++ Models/Studente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Studenti.Models
+{
+    internal class Studente
+    {
+        public string Matricola;
+        public string Cognome;
+        public string Nome;
+
+        public Studente(string Matricola, string Cognome, string Nome)
+        {
+            this.Matricola = Matricola;
+            this.Cognome = Cognome;
+            this.Nome = Nome;
+        }
+    }
+}
